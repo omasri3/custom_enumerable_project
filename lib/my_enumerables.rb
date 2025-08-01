bae4b1ab416f156d6d7d1b466a0cc1751a7a3fb2
Omar Masri <16193809+omasri3@users.noreply.github.com>
2025-08-01 00:41:00 -0400

    all enumerable methods created and tested!

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,74 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    self.my_each do |elem|
+      yield(elem, index)
+      index += 1
+    end
+  end
+
+  def my_select
+    sub_selection = []
+    self.my_each do |elem|
+      sub_selection << elem if yield(elem)
+    end
+    sub_selection
+  end
+
+  def my_all?
+    self.my_each do |elem|
+      return false unless yield(elem)
+    end
+    true
+  end
+
+  def my_any?
+    self.my_each do |elem|
+      return true if yield(elem)
+    end
+    false
+  end
+
+  def my_none?
+    self.my_each do |elem|
+      return false if yield(elem)
+    end
+    true
+  end
+
+  def my_count
+    count = 0
+    if block_given?
+      self.my_each do |elem|
+        count += 1 if yield(elem)
+      end
+    else
+      self.my_each do |elem|
+        count += 1
+      end
+    end
+    count
+  end
+
+  def my_map
+    mapped_arr = []
+    if block_given?
+      self.my_each do |elem|
+        mapped_arr << yield(elem)
+      end
+    end
+    mapped_arr
+  end
+
+  def my_inject(initial_val)
+    final_value = initial_val
+    self.my_each do |elem|
+      final_value = yield(final_value, elem)
+    end
+    final_value
+  end
+
 end
 
 # You will first have to define my_each
@@ -8,4 +77,9 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for elem in self
+      yield(elem)
+    end
+  end
 end
